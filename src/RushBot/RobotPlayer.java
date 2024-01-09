50bd374b2cf0f7144864b5ea83ccbbefdd6f04c2
WendidIask <Aiden_wen2010@outlook.com>
2024-01-09 23:13:07 +1100

    new

--- src/RushBot/RobotPlayer.java
+++ src/RushBot/RobotPlayer.java
@@ -3,8 +3,30 @@ package RushBot;
 import battlecode.common.*;
 
 import java.util.*;
+
+/**
+ * RobotPlayer is the class that describes your main robot strategy.
+ * The run() method inside this class is like your main function: this is what we'll call once your robot
+ * is created!
+ */
 public strictfp class RobotPlayer {
+
+    /**
+     * We will use this variable to count the number of turns this robot has been alive.
+     * You can use static variables like this to save any information you want. Keep in mind that even though
+     * these variables are static, in Battlecode they aren't actually shared between your robots.
+     */
+    static int turnCount = 0;
+
+    /**
+     * A random number generator.
+     * We will use this RNG to make some random moves. The Random class is provided by the java.util.Random
+     * import at the top of this file. Here, we *seed* the RNG with a constant number (6147); this makes sure
+     * we get the same sequence of numbers every time this code is run. This is very useful for debugging!
+     */
     static Random rng = new Random(6147);
+
+    /** Array containing all the possible movement directions. */
     static final Direction[] directions = {
             Direction.NORTH,
             Direction.NORTHEAST,
@@ -16,12 +38,32 @@ public strictfp class RobotPlayer {
             Direction.NORTHWEST,
     };
 
+    /**
+     * run() is the method that is called when a robot is instantiated in the Battlecode world.
+     * It is like the main function for your robot. If this method returns, the robot dies!
+     *
+     * @param rc  The RobotController object. You use it to perform actions from this robot, and to get
+     *            information on its current status. Essentially your portal to interacting with the world.
+     **/
+    @SuppressWarnings("unused")
     public static void run(RobotController rc) throws GameActionException {
+
+        // Hello world! Standard output is very useful for debugging.
+        // Everything you say here will be directly viewable in your terminal when you run a match!
+        //System.out.println("I'm alive");
+
+        // You can also use indicators to save debug notes in replays.
+        //rc.setIndicatorString("Hello world!");
         rng = new Random(rc.getID());
         MapLocation targetCell = new MapLocation(-1, -1);
         int targetTurnsSpent = 0;
         boolean exactTarget = false;
         while (true) {
+            // This code runs during the entire lifespan of the robot, which is why it is in an infinite
+            // loop. If we ever leave this loop and return from run(), the robot dies! At the end of the
+            // loop, we call Clock.yield(), signifying that we've done everything we want to do.
+
+            turnCount += 1;  // We have now been alive for one more turn!
             ++targetTurnsSpent;
 
             // Try/catch blocks stop unhandled exceptions, which cause your robot to explode.
